17e3f1dc60d00e2a1b90be9fead2178e95a3b68f
liutuo <lioutall@163.com>
2020-02-03 14:42:24 +0800

    apollo client support

--- microservices/microservices-apollo/src/test/java/TestApollo.java
+++ microservices/microservices-apollo/src/test/java/TestApollo.java
@@ -1,12 +1,26 @@
 import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.ConfigService;
+import io.netty.util.internal.StringUtil;
 
 public class TestApollo {
     public static void main(String[] args) {
-        Config config = ConfigService.getAppConfig(); //config instance is singleton for each namespace and is never null
-        String someKey = "someKeyFromDefaultNamespace";
-        String someDefaultValue = "someDefaultValueForTheKey";
+        //启动参数添加 -Denv=LPT
+        System.setProperty("apollo.cluster", "test1");
+        
+        //config instance is singleton for each namespace and is never null
+        Config config = ConfigService.getConfig("component");
+
+        String someKey = "testKey";
+        String someDefaultValue = "nothing";
         String value = config.getProperty(someKey, someDefaultValue);
+        System.out.println(value);
+
+
+        Config config1 = ConfigService.getConfig("application");
+        someKey = "testKey";
+        someDefaultValue = "nothing";
+        value = config1.getProperty(someKey, someDefaultValue);
+        System.out.println(value);
     }
 
 }
