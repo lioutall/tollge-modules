647095f7a1fdeca7d58a9e0964cc58dbea516696
liutuo <lioutall@163.com>
2024-08-05 15:28:03 +0800

    Update Tester.java

--- data/curd-reactive-pg/src/test/java/test/curd/Tester.java
+++ data/curd-reactive-pg/src/test/java/test/curd/Tester.java
@@ -1,8 +1,14 @@
 package test.curd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.tollge.MainVerticle;
+import com.tollge.common.util.Properties;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.jackson.DatabindCodec;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestOptions;
 import io.vertx.ext.unit.TestSuite;
@@ -10,6 +16,7 @@ import io.vertx.ext.unit.report.ReportOptions;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author toyer
@@ -32,11 +39,21 @@ public class Tester {
         });
 
         suite.test("testDB", context -> {
+            String jsonFormat = Properties.getString("application", "json.format", "yyyy-MM-dd HH:mm:ss");
+            ObjectMapper objectMapper = DatabindCodec.mapper();
+            JavaTimeModule module = new JavaTimeModule();
+            LocalDateTimeDeserializer localDateTimeDeserializer =  new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(jsonFormat));
+            module.addDeserializer(LocalDateTime.class, localDateTimeDeserializer);
+            LocalDateTimeSerializer localDateTimeSerializer = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(jsonFormat));
+            module.addSerializer(localDateTimeSerializer);
+            objectMapper.registerModule(module);
+
             Async async = context.async();
-            vertx.eventBus().request("biz://tt/fetchOne", new JsonObject().put("a", "bbb"), reply -> {
+            vertx.eventBus().request("biz://tt/one", new JsonObject().put("a", 1), reply -> {
                 if(reply.succeeded()) {
                     System.out.println("final: "+reply.result().body());
                 } else {
+                    reply.cause().printStackTrace();
                     System.err.println(reply.cause().getMessage());
                 }
                 async.complete();
